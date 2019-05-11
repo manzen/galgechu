d16028484ead7c068fde30a9479cd07d8e3a56bf
irie-shinnosuke <irie-shinnosuke@kayac.com>
2019-05-11 19:14:15 +0900

    ボタンを追加

---
+++ frontend/src/components/ButtonItem.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+    .button-item
+        template(v-if="buttonType === 'button'")
+            button.button {{ text }}
+        template(v-else)
+            router-link.button(:to="link") {{ text }}
+</template>
+
+<script>
+export default {
+    name: "ButtonItem",
+    props: {
+        buttonType: {
+            type: String,
+            default: "link"
+        },
+        link: {
+            type: String,
+            default: "/"
+        },
+        text: {
+            type: String,
+            default: "ボタン"
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+    display: block;
+    width: 250px;
+    height: 60px;
+    line-height: 60px;
+    border-radius: 13px;
+    font-size: 20px;
+    color: #fff;
+    background-color: #FFA2A2;
+    text-decoration: none;
+    text-align: center;
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
   .home
     header-item
+    button-item(button-type="link")
     img(src="../assets/logo.png" alt="Vue logo" @click="addCount")
     p {{ count }}
     HelloWorld(msg="Welcome to Your Vue.js App")
@@ -11,21 +12,23 @@ import { mapState, mapActions } from "vuex";
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 import HeaderItem from "../components/HeaderItem";
+import ButtonItem from "../components/ButtonItem";
 
 export default {
-  name: "home",
-  components: {
-    HeaderItem,
-    HelloWorld
-  },
-  computed: {
-    ...mapState(["count"])
-  },
-  methods: {
-    ...mapActions(["increment"]),
-    addCount() {
-      this.increment();
+    name: "home",
+    components: {
+        ButtonItem,
+        HeaderItem,
+        HelloWorld
+    },
+    computed: {
+        ...mapState(["count"])
+    },
+    methods: {
+        ...mapActions(["increment"]),
+        addCount() {
+            this.increment();
+        }
     }
-  }
 };
 </script>
