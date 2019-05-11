60c282a47e932833e1721aab8e8cb0d0d2a54e9c
irie-shinnosuke <irie-shinnosuke@kayac.com>
2019-05-11 18:58:41 +0900

    headerを追加

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
   .home
+    header-item
     img(src="../assets/logo.png" alt="Vue logo" @click="addCount")
     p {{ count }}
     HelloWorld(msg="Welcome to Your Vue.js App")
@@ -9,10 +10,12 @@
 import { mapState, mapActions } from "vuex";
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderItem from "../components/HeaderItem";
 
 export default {
   name: "home",
   components: {
+    HeaderItem,
     HelloWorld
   },
   computed: {
