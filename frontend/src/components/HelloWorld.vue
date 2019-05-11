145dee74337e4536d263bf05b4dd604c3ebae344
irie-shinnosuke <irie-shinnosuke@kayac.com>
2019-05-11 18:26:21 +0900

    vueのfrontend環境を追加

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+    .hello
+      p.text Hello!World.
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.text {
+  font-style: italic;
+  font-weight: bold;
+}
+</style>
